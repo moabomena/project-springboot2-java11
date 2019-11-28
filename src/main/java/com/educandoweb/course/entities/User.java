40286469077a701d680dbc53a8f8697a904f7810
Moab Omena <1moabomena@gmail.com>
2019-11-28 18:04:41 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,9 +2,17 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+ @Entity
+ public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -87,9 +95,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-		
-	
-	
-	
 	
 }
